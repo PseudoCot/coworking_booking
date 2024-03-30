8191a5c812424e44e8494564e3831983ecbaf542
psevdokot <youkos00@mail.ru>
2024-03-30 18:57:54 +0500

    Init

---
+++ src/pages/error-screen.tsx
@@ -0,0 +1,18 @@
+import './error-screen.css';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../routes';
+import { ErrorCodesDesc } from '../consts';
+import { useAppDispatch, useAppSelector } from '../hooks';
+import { getErrorCode } from '../store/error-process/selectors';
+import { useEffect } from 'react';
+import { setErrorCode } from '../store/error-process/error-process';
+
+export type ErrorScreenProps = {
+  errorStatusCode?: number;
+};
+
+export default function ErrorScreen({ errorStatusCode = -1 }: ErrorScreenProps): JSX.Element {
+  return (
+    <div>An error occured</div>
+  );
+}
