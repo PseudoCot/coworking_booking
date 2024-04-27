d634b0b46fae9aeee82cc35eccbb8e74074d7f0c
psevdokot <youkos00@mail.ru>
2024-04-27 18:30:14 +0500

    Implement header, footer and layout components

---
+++ src/components/layout.tsx
@@ -0,0 +1,19 @@
+import { PropsWithChildren } from 'react';
+import Header from './header';
+import Footer from './footer';
+
+type LayoutProps = PropsWithChildren;
+
+export default function Layout({ children }: LayoutProps): JSX.Element {
+  return (
+    <div className='container'>
+      <Header />
+
+      <main className="main">
+        {children}
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
