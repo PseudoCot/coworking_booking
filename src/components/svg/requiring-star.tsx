c841e71cfb70611ea8e6197c75bc77b25d8f9fca
psevdokot <youkos00@mail.ru>
2024-05-13 23:05:31 +0500

    Add new icons

---
+++ src/components/svg/requiring-star.tsx
@@ -0,0 +1,14 @@
+type RequiringStarSVGProps = {
+  classNames?: string;
+};
+
+export default function RequiringStarSVG({ classNames }: RequiringStarSVGProps): JSX.Element {
+  return (
+    <svg className={classNames} width="10" height="11" viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M1 1L8.99988 10" stroke="#B71C1C" />
+      <path d="M1 10L9 1" stroke="#B71C1C" />
+      <path d="M5 0L5 11" stroke="#B71C1C" />
+      <line y1="5.5" x2="10" y2="5.5" stroke="#B71C1C" />
+    </svg>
+  );
+}
