f0d8d24eec0e306f4ff89f7464e893cfdb28d85a
psevdokot <youkos00@mail.ru>
2024-05-17 00:04:54 +0500

    Update calendar styles

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -2,10 +2,10 @@ import { useState } from 'react';
 import { DateTime, Interval } from 'luxon';
 import classnames from 'classnames';
 
-const WEEKDAYS = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
-// const MONTHS = {
-//   ''
-// }
+// const WEEKDAYS = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'] as const;
+const MONTHS = ['ЯНВАРЬ', 'ФЕВРАЛЬ', 'МАРТ', 'АПРЕЛЬ', 'МАЙ', 'ИЮНЬ',
+  'ИЮЛЬ', 'АВГУСТ', 'СЕНТЯБРЬ', 'ОКТЯБРЬ', 'НОЯБРЬ', 'ДЕКАБРЬ'] as const;
+const HOLIDAYS = [[1, 2, 3, 4, 5, 6, 7, 8], [23], [8], [], [1, 9], [12], [], [], [], [], [4], [31]];
 
 // export type CalendarProps = {
 
@@ -33,41 +33,45 @@ export default function Calendar(): JSX.Element {
   };
 
   return (
-    <div className="calendar-container">
-      <div className="calendar-it">
-        <div className="calendar-controls">
-          <button className="calendar-controls btn-reset" onClick={() => handlePreviousMonthClick()}>
-            «
-          </button>
-          <button className="calendar-controls calendar-controls-today btn-reset" onClick={() => handleResetMonthClick()}>
-            {firstDayOfActiveMonth.monthShort} {firstDayOfActiveMonth.year}
-          </button>
-          <button className="calendar-controls btn-reset" onClick={() => handleNextMonthClick()}>
-            »
-          </button>
-        </div>
-        <div className="calendar-weeks-grid">
-          {WEEKDAYS.map((weekDay) => (
-            <div key={weekDay} className="calendar-weeks-grid-cell">
-              {weekDay}
-            </div>
-          ))}
-        </div>
-        <div className="calendar-grid">
-          {daysOfMonth.map((dayOfMonth) => (
+    <div className="calendar__self">
+      <div className="calendar__self-controls">
+        <button className="calendar__self-controls-left-btn btn-reset" onClick={() => handlePreviousMonthClick()} />
+        <button className="calendar__self-controls-today-btn btn-reset" onClick={() => handleResetMonthClick()}>
+          {MONTHS[firstDayOfActiveMonth.month - 1]} {firstDayOfActiveMonth.year}
+        </button>
+        <button className="calendar__self-controls-right-btn btn-reset" onClick={() => handleNextMonthClick()} />
+      </div>
+      <div className="calendar__self-grid">
+        {/* {WEEKDAYS.map((weekDay) => (
+          <div key={weekDay} className="calendar__self-grid-weekday-cell">
+            {weekDay}
+          </div>
+        ))} */}
+        {daysOfMonth.map((dayOfMonth, index) => {
+          const isDayHoliday = HOLIDAYS[dayOfMonth.month - 1].includes(dayOfMonth.day);
+          return (
             <button
               key={`${dayOfMonth.day}/${dayOfMonth.month}`}
-              className={classnames({
-                'calendar-grid-cell': true,
-                'calendar-grid-cell-inactive': dayOfMonth.month !== firstDayOfActiveMonth.month,
-                'calendar-grid-cell-active': activeDay?.toISODate() === dayOfMonth.toISODate(),
+              className={classnames('calendar__self-grid-cell btn-reset', {
+                'calendar__self-grid-cell--inactive': dayOfMonth.month !== firstDayOfActiveMonth.month,
+                'calendar__self-grid-cell--active': dayOfMonth.toISODate() === activeDay?.toISODate(),
               })}
               onClick={() => setActiveDay(dayOfMonth)}
             >
-              {dayOfMonth.day}
+              <div className="calendar__self-grid-cell-top">
+                {(dayOfMonth.day === 1 || index === 0) &&
+                  <span className='calendar__self-grid-cell-month'>{MONTHS[dayOfMonth.month - 1]}</span>}
+                <span className={classnames({ 'calendar__self-grid-cell-holiday': isDayHoliday })}>
+                  {dayOfMonth.day}
+                </span>
+              </div>
+              <div className="calendar__self-grid-cell-bottom">
+                {isDayHoliday &&
+                  <div className="calendar__self-grid-cell-message">Нет доступных слотов<br /> для бронирования</div>}
+              </div>
             </button>
-          ))}
-        </div>
+          );
+        })}
       </div>
     </div>
   );
