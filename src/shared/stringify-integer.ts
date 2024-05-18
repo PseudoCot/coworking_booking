1d16bfaeaa26a296e34bd68b962c34cade8c9885
psevdokot <youkos00@mail.ru>
2024-05-18 23:33:51 +0500

    Fix stringify integer method 0 as prop bug

--- src/shared/stringify-integer.ts
+++ src/shared/stringify-integer.ts
@@ -1,4 +1,8 @@
 export default function stringifyInteger(int: number, stringLength = 2) {
+  if (int === 0) {
+    return '00';
+  }
+
   const diff = stringLength - Math.ceil(Math.log10(int + 1));
   return diff > 0
     ? `${Array.from({ length: stringLength - diff }, () => '0').join('')}${int}`
