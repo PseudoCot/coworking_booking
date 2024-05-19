e79186384be6c3579f8b798925481949a2e9ae56
psevdokot <youkos00@mail.ru>
2024-05-19 19:27:13 +0500

    Delete previous version of calendar component

--- src/shared/get-days-in-month.ts
+++
@@ -1,7 +0,0 @@
-export default function getDaysInMonth(year: number, month: number): number {
-  // const presentDay = new Date();
-  // const presentYear = presentDay.getFullYear();
-  // const presentMonth = presentDay.getMonth();
-  // return 32 - new Date(year ?? presentYear, month ?? presentMonth, 32).getDate();
-  return 32 - new Date(year, month, 32).getDate();
-}
