26301064b4770ecb7a20ce9382c5052e29d8af80
psevdokot <youkos00@mail.ru>
2024-05-19 20:56:22 +0500

    Update submit form styles

--- src/components/submit-form.tsx
+++ src/components/submit-form.tsx
@@ -3,6 +3,7 @@ type SubmitFormProps = {
   question: string;
   dismissText: string;
   submitText: string;
+
   onDismiss: () => void;
   onSubmit: () => void;
 };
@@ -10,17 +11,19 @@ type SubmitFormProps = {
 export default function SubmitForm({ title, question, dismissText, submitText, onDismiss: handleDismiss,
   onSubmit: handleSubmit }: SubmitFormProps): JSX.Element {
   return (
-    <form className="user-acc__submit-form submit-form cb-form" action="#" onSubmit={handleSubmit}>
-      <div className="submit-form__top cb-form-top">
-        <h2 className="submit-form__title cb-form-title title-reset">{title}</h2>
-      </div>
-      <div className="submit-form__bottom cb-form-bottom">
-        <h3 className="submit-form__sub-title">{question}</h3>
-        <div className="submit-form__btns">
-          <button className="submit-form__cancel-btn btn-reset" onClick={handleDismiss}>{dismissText}</button>
-          <button className="submit-form__submit-btn btn-reset">{submitText}</button>
+    <div className="submit-form__wrapper">
+      <form className="submit-form cb-form" action="#" onSubmit={handleSubmit}>
+        <div className="submit-form__top cb-form-top">
+          <h2 className="submit-form__title cb-form-title title-reset">{title}</h2>
         </div>
-      </div>
-    </form>
+        <div className="submit-form__bottom cb-form-bottom">
+          <h3 className="submit-form__sub-title">{question}</h3>
+          <div className="submit-form__btns">
+            <button className="submit-form__cancel-btn btn-reset" onClick={handleDismiss}>{dismissText}</button>
+            <button className="submit-form__submit-btn btn-reset">{submitText}</button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
