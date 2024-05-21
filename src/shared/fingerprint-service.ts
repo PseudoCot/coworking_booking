ed52214ca24b4faa9aee8adb47824f1e557faa34
psevdokot <youkos00@mail.ru>
2024-05-22 00:24:24 +0500

    Update fingerprint service

--- src/shared/fingerprint-service.ts
+++ src/shared/fingerprint-service.ts
@@ -3,17 +3,21 @@ import FingerprintJS, { Agent } from '@fingerprintjs/fingerprintjs';
 
 export default class FingerprintService {
   private readonly fpServiceLoading: Promise<Agent>;
+  private readonly fpIdLoading: Promise<string>;
 
   constructor() {
     this.fpServiceLoading = FingerprintJS.load();
+    this.fpIdLoading = this.createFingerprintId();
   }
 
-  public async createFingerprintId(): Promise<string> {
+  private async createFingerprintId(): Promise<string> {
     const fpService = await this.fpServiceLoading;
     const fingerprint = await fpService.get();
-    // eslint-disable-next-line no-console
-    console.log(fingerprint);
     return fingerprint.visitorId;
+  }
+
+  public async getFingerprintId(): Promise<string> {
+    return this.fpIdLoading;
   }
 }
 
