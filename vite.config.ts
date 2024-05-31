00539052635f836036617deec0f1c3b9a9f0502a
psevdokot <youkos00@mail.ru>
2024-05-31 21:20:59 +0500

    Update vite config for docker container

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8000, // port which we will use in docker
+    watch: { // hot reload
+      usePolling: true
+    }
+  },
   // test: {
   //   globals: true,
   //   environment: 'jsdom',
