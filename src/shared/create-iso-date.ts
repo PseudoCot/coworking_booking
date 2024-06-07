86b1be92c801d47585a58b4ccc69c3294083f212
psevdokot <youkos00@mail.ru>
2024-06-07 19:51:18 +0500

    Fix booking bug

--- src/shared/create-iso-date.ts
+++ src/shared/create-iso-date.ts
@@ -1,3 +1,4 @@
 export default function createISODate(isoDate: string, hour: string, minute: string, second = '00') {
-  return `${isoDate}T${hour}:${minute}:${second}.000Z`;
+  // return `${isoDate}T${hour}:${minute}:${second}.000Z`;
+  return `${isoDate}T${hour}:${minute}:${second}`;
 }
