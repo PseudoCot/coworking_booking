f9823257ead8e77ebfd35c214363ef15c857ca0f
psevdokot <youkos00@mail.ru>
2024-06-11 18:24:49 +0500

    Fix redirecting unknown user from private page

--- src/components/private-route.tsx
+++ src/components/private-route.tsx
@@ -1,16 +1,21 @@
 import { Navigate } from 'react-router-dom';
 import { PropsWithChildren } from 'react';
-import { AuthStatuses } from '../consts';
+import { AuthStatuses, FetchingStatuses } from '../consts';
 import { useAppSelector } from '../hooks';
 import { getAuthStatus } from '../store/user-process/selectors';
 import { AppRoutes } from '../routes';
+import { useUserFetchingStatus } from '../hooks/use-user-fetching-status';
 
 export type PrivateRouteProps = PropsWithChildren;
 
 export default function PrivateRoute({ children }: PrivateRouteProps): JSX.Element {
   const authStatus = useAppSelector(getAuthStatus);
+  const fetchingStatus = useUserFetchingStatus('refreshFetchingStatus');
 
-  return authStatus === AuthStatuses.Auth || import.meta.env.VITE_DEV_ALLOW_PRIVATE_PAGES === 'false' //temp
+  return authStatus === AuthStatuses.Auth
+    || fetchingStatus === FetchingStatuses.Pending
+    || import.meta.env.VITE_DEV_ALLOW_PRIVATE_PAGES === 'true'
+
     ? children as JSX.Element
     : <Navigate to={AppRoutes.Login.FullPath} />;
 }
