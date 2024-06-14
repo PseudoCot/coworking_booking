9e3a22bbbb6479fb630280e7e772ebdd8fe1905b
psevdokot <youkos00@mail.ru>
2024-06-14 15:38:27 +0500

    Fix schedule data bug

--- src/shared/sorted-array-by-element-field.ts
+++ src/shared/sorted-array-by-element-field.ts
@@ -1,9 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 export default function sortedArrayByElementField<R = object>(list: object[], field: string | number): R[] {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  const result = list.reduce((res, elem) => (res[elem[field]] = elem), {});
+  const result = list.reduce((res, elem) => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    res[elem[field]] = elem;
+    return res;
+  }, {});
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return Object.values(result);
