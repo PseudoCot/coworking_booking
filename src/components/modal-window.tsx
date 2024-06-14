44acb5943810622fafad539e64bcf771ca5f6a55
psevdokot <youkos00@mail.ru>
2024-06-14 20:26:19 +0500

    Fix page scroll while a model window is open

--- src/components/modal-window.tsx
+++ src/components/modal-window.tsx
@@ -1,11 +1,21 @@
 import classNames from 'classnames';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect } from 'react';
 
 type ModalWindowProps = PropsWithChildren<{
   show: boolean;
 }>;
 
 export default function ModalWindow({ show, children }: ModalWindowProps): JSX.Element {
+  useEffect(() => {
+    if (show) {
+      document.body.style.overflowY = 'hidden';
+      document.body.style.paddingRight = '15px';
+    } else {
+      document.body.style.overflowY = 'auto';
+      document.body.style.paddingRight = '0';
+    }
+  }, [show]);
+
   return (
     <div className={classNames('modal-window', { 'modal-window--show': show })}>
       {show &&
